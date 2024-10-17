96fa649d9e25ea0fa6c1f429f0637942cd0bba97
Polina Cherkasova <polina.c@live.com>
2024-10-17 14:03:24 -0700

    -

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 ## Extension
 
+All commands are for the directory `extension`
+
 ### Run locally
 
-Options:
+```
+npm run compile
+```
+
+Then one of:
 1. Open [extension.ts](extension/src/extension.ts) and press F5
 2. Run and Debug > Run Extension
--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -2,13 +2,15 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import { CustomSidebarViewProvider } from './customSidebarViewProvider';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "genui-panel" is now active!');
+	console.log('The extension "genui-panel" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -20,7 +22,33 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	const provider = new CustomSidebarViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			CustomSidebarViewProvider.viewType,
+			provider
+		)
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("genui-panel.menu.view", () => {
+			const message = "Menu/Title of extension is clicked !";
+			vscode.window.showInformationMessage(message);
+		})
+	);
+
+	// Command has been defined in the package.json file
+	// Provide the implementation of the command with registerCommand
+	// CommandId parameter must match the command field in package.json
+	let openWebView = vscode.commands.registerCommand('genui-panel.openview', () => {
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Command " Sidebar View [genui-panel.openview] " called.');
+	});
+
+	context.subscriptions.push(openWebView);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
