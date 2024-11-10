5a4eba90c20e9b84aa240fd54eb8837bef722903
Polina Cherkasova <polina.c@live.com>
2024-11-09 20:22:41 -0800

    Update _scrolled_text.dart

--- ui/lib/screens/content/_scrolled_text.dart
+++ ui/lib/screens/content/_scrolled_text.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ScrolledText extends StatelessWidget {
-  ScrolledText(this.text, {super.key}) {
-    content = text.split('\n');
-  }
+  ScrolledText(this.text, {super.key});
+
   final String text;
-  late final List<String> content;
-  final int maxLength = 650;
 
   @override
   Widget build(BuildContext context) {
